7e5304aa07fb71acbcd9b29eaf94ffa617e9b7fc
takeshi furusawa <djpdjp0517@gmail.com>
2020-09-16 01:43:26 +0000

    add : バリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  mount_uploader :picture, ImageUploader
+  with_options presence: true do
+    validates :title
+    validates :music
+    validates :picture
+    validates :type
+  end
 end
