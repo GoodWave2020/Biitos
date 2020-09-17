b889f3535aaf8e9a593aff8eff832425145e35aa
takeshi furusawa <djpdjp0517@gmail.com>
2020-09-17 02:52:58 +0000

    add : プロフィール画面を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
+  get '/users/profile/:id', to: 'users#show', as: 'user'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
