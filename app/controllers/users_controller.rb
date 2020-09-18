9066d72b0e8a3d14d34af1fae7c868b3b052497a
takeshi furusawa <djpdjp0517@gmail.com>
2020-09-18 07:16:11 +0000

    add : フォロワーとフォローイングをユーザーごとに確認できる機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+  before_action :set_user
   def show
+  end
+
+  def following
+  end
+
+  def followers
+  end
+  private
+  def set_user
     @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   get '/users/profile/:id', to: 'users#show', as: 'user'
-
+  get '/users/following/:id', to: 'users#following', as: 'following'
+  get '/users/followers/:id', to: 'users#followers', as: 'followers'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
