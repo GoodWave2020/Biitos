4f00eab5297efb289f2b9f98fd7ed68b48c2066a
takeshi furusawa <djpdjp0517@gmail.com>
2020-10-03 20:41:23 +0000

    change ; フロントデザインを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 10.times do |n|
   User.create!(name: "Mr.tuner#{n}",
               email: "number#{n}@tuner.com",
-              icon: 'app/assets/images/fallback/show_icon_default.png',
               profile: 'test',
               artist_type: 'Tuner',
               password: 'password',
@@ -11,7 +10,6 @@ end
 10.times do |n|
   User.create!(name: "Mr.voicer#{n}",
               email: "number#{n}@voicer.com",
-              icon: 'app/assets/images/fallback/show_icon_default.png',
               profile: 'test',
               artist_type: 'Voicer',
               password: 'password',
@@ -20,8 +18,7 @@ end
 end
 10.times do |n|
   Post.create!(title: "Tune#{n}",
-              music:File.open('app/assets/audios/velvet_extended.wav'),
-              picture:File.open('app/assets/images/fallback/thumb_default.png'),
+              music:File.open('app/assets/audios/velvet_extended.mp3'),
               music_type: 'Tune',
               user_id: User.find_by(name: "Mr.tuner#{n}").id,
               genre: 0
@@ -29,8 +26,7 @@ end
 end
 10.times do |n|
   Post.create!(title: "Voice#{n}",
-              music:File.open('app/assets/audios/velvet_extended.wav'),
-              picture:File.open('app/assets/images/fallback/thumb_default.png'),
+              music:File.open('app/assets/audios/velvet_extended.mp3'),
               music_type: 'Voice',
               user_id: User.find_by(name: "Mr.voicer#{n}").id,
               genre: 0
@@ -38,8 +34,7 @@ end
 end
 10.times do |n|
   Post.create!(title: "Collabolated#{n}",
-              music:File.open('app/assets/audios/velvet_extended.wav'),
-              picture:File.open('app/assets/images/fallback/thumb_default.png'),
+              music:File.open('app/assets/audios/velvet_extended.mp3'),
               music_type: 'Collabolated',
               user_id: User.find_by(name: "Mr.tuner#{n}").id,
               genre: 0
