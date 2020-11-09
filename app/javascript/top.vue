b334ca22c69e72a25a3044914f1bc78f5e277517
takeshi furusawa <djpdjp0517@gmail.com>
2020-11-09 10:38:25 +0000

    add : #25 トップ画面にアニメーションを追加

--- app/javascript/top.vue
+++ app/javascript/top.vue
@@ -1,5 +1,15 @@
 <template>
-  
+  <div>
+    <transition name='fade' mode='out-in' appear>
+        <div class="top-title">Biitos</div>
+    </transition>
+    <transition name='fade2' mode='out-in' appear
+        @before-enter="beforeEnter"
+        @after-enter="afterEnter"
+    >
+        <div class="description">最高の出会いと最高の音楽を</div>
+    </transition>
+  </div>
 </template>
 
 <script>
@@ -9,5 +19,22 @@ export default {
 </script>
 
 <style>
-
+  .fade-enter {
+    opacity: 0;
+  }
+  .fade-enter-active {
+    transition: opacity 1s;
+  }
+  .fade-enter-to {
+    opacity: 1;
+  }
+  .fade2-enter {
+    opacity: 0;
+  }
+  .fade2-enter-active {
+    transition: opacity 5s;
+  }
+  .fade2-enter-to {
+    opacity: 1;
+  }
 </style>
